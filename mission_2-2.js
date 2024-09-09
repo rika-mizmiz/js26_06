e11f1fbdeac174f1ad3647d5e1c8f06ae75396df
rika-mizmiz <107254553+rika-mizmiz@users.noreply.github.com>
2024-09-10 01:04:54 +0900

    mission_2

---
+++ mission_2-2.js
@@ -0,0 +1,7 @@
+// var text = prompt("挨拶を入力してください");
+// var object = document.getElementById("greeting");
+// object.innerText = text;
+
+var text = prompt("挨拶を入力してください", "こんにちは");
+var object = document.getElementById("greeting");
+object.innerText = text;
